5346a4ff7ee197c92a22053013089d648c7fe00b
andresscs <andresscs@berkeley.edu>
2025-01-29 20:55:35 -0800

    Add data for 1450kHz signal frequency

--- Lab1Functions/SignalSamplerLoop_Lab1.py
+++ Lab1Functions/SignalSamplerLoop_Lab1.py
@@ -3,8 +3,8 @@ import numpy as np
 
 coeffs = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2047])
 
-offset = 0.0005 #Volts
-sample_rates = np.array([1.3e6,2e6,3.1e6])
+offset = 0 #Volts
+sample_rates = np.array([3.1e6,2e6,1.3e6])
 save_path = "../Lab1Data/Section5_2/"
 for i, rate in enumerate(sample_rates):
     print(f"Capturing data at {int(rate / 1e3)} kHz sample rate...")
@@ -13,7 +13,7 @@ for i, rate in enumerate(sample_rates):
     time_data = sdrdata.capture_data(nblocks = 5, nsamples = 2048)  # Capture data
     sdrdata.close()  # Close SDR before the next iteration
     
-    filename = f"{save_path}lab_1_data_480khz_{int(rate / 1e3)}khz.npz"
+    filename = f"{save_path}lab_1_data_1450khz_{int(rate / 1e3)}khz.npz"
 
     np.savez(filename, time_data = time_data, sample_rate = rate, offset = offset)
 
