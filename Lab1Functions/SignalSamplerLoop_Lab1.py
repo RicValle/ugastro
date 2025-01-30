159d5a308975ee4ca7c9c57923cdbde22e90e225
andresscs <andresscs@berkeley.edu>
2025-01-30 12:18:01 -0800

    Data for 480kHz and 200kHz

--- Lab1Functions/SignalSamplerLoop_Lab1.py
+++ Lab1Functions/SignalSamplerLoop_Lab1.py
@@ -4,7 +4,7 @@ import numpy as np
 coeffs = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2047])
 
 offset = 0 #Volts
-sample_rates = np.array([3.1e6,2e6,1.3e6])
+sample_rates = np.array([1e6,1.25e6,1.5e6,1.75e6,2e6,2.25e6,2.5e6,2.75e6,3e6])
 save_path = "../Lab1Data/Section5_2/"
 for i, rate in enumerate(sample_rates):
     print(f"Capturing data at {int(rate / 1e3)} kHz sample rate...")
@@ -13,7 +13,7 @@ for i, rate in enumerate(sample_rates):
     time_data = sdrdata.capture_data(nblocks = 5, nsamples = 2048)  # Capture data
     sdrdata.close()  # Close SDR before the next iteration
     
-    filename = f"{save_path}lab_1_data_1450khz_{int(rate / 1e3)}khz.npz"
+    filename = f"{save_path}lab_1_data_200khz_{int(rate / 1e3)}khz.npz"
 
     np.savez(filename, time_data = time_data, sample_rate = rate, offset = offset)
 
