f9c32f6b39794bf0530c4c66126ebfd7e4a2e0b2
andres814 <andresscs@berkeley.edu>
2025-01-29 17:45:38 -0800

    Rename lab1AnalysisHelper to Lab1_SignalSampler.py

--- Lab1_SignalSampler.py
+++ Lab1_SignalSampler.py
@@ -18,3 +18,12 @@ time_data_zone_3 = sdr_filtered_zone_3.capture_data()
 # 
 np.savez("lab_1_data_1500khz_1300khz", time_data_zone_3) #, time_data_zone_2, time_data_zone_3)
 
+sample_rates = np.array([])
+for i, rate in enumerate(sample_rates):
+    print(f"Capturing data at {rate / 1e6:.1f} MHz sample rate...")
+    
+    sdrdata = ugradio.sdr.SDR(sample_rate=rate, fir_coeffs=coeffs)  # Initialize SDR
+    time_data = sdrdata.capture_data()  # Capture data
+    sdrdata.close()  # Close SDR before the next iteration
+    
+    np.savez("lab_1_data_1500khz_1300khz", time_data)
