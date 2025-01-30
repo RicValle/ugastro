d1684c8d96d053b058dd65a42a8a67f3ef4de00d
andresscs <andresscs@berkeley.edu>
2025-01-29 20:18:34 -0800

    took 480kHz data for section 5.2

--- Lab1Functions/SignalSamplerLoop_Lab1.py
+++ Lab1Functions/SignalSamplerLoop_Lab1.py
@@ -3,15 +3,17 @@ import numpy as np
 
 coeffs = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2047])
 
-sample_rates = np.array([1.5e6])
+offset = 0.0005 #Volts
+sample_rates = np.array([1e6,2e6,3e6])
 save_path = "../Lab1Data/Section5_2/"
 for i, rate in enumerate(sample_rates):
     print(f"Capturing data at {int(rate / 1e3)} kHz sample rate...")
     
     sdrdata = ugradio.sdr.SDR(sample_rate=rate, fir_coeffs=coeffs)  # Initialize SDR
-    captured_time_data = sdrdata.capture_data(nblocks = 1, nsamples = 2048)  # Capture data
+    time_data = sdrdata.capture_data(nblocks = 5, nsamples = 2048)  # Capture data
     sdrdata.close()  # Close SDR before the next iteration
     
-    filename = f"{save_path}lab_1_data_480khz_{int(rate / 1e3)}khz.npz"
+    filename = f"{save_path}lab_1_data_480khz_{int(rate / 1e3)}khz_offset.npz"
 
-    np.savez(filename, time_data = captured_time_data, sample_rate = rate)
+    np.savez(filename, time_data = time_data, sample_rate = rate, offset = offset)
+
