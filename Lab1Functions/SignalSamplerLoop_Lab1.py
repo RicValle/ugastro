0dc69c5bc974af8f2f75f543365ee0998adf181f
andresscs <andresscs@berkeley.edu>
2025-01-29 20:29:18 -0800

    Add more 480 kHz data not offset

--- Lab1Functions/SignalSamplerLoop_Lab1.py
+++ Lab1Functions/SignalSamplerLoop_Lab1.py
@@ -4,7 +4,7 @@ import numpy as np
 coeffs = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2047])
 
 offset = 0.0005 #Volts
-sample_rates = np.array([1e6,2e6,3e6])
+sample_rates = np.array([1.3e6,2e6,3.1e6])
 save_path = "../Lab1Data/Section5_2/"
 for i, rate in enumerate(sample_rates):
     print(f"Capturing data at {int(rate / 1e3)} kHz sample rate...")
@@ -13,7 +13,7 @@ for i, rate in enumerate(sample_rates):
     time_data = sdrdata.capture_data(nblocks = 5, nsamples = 2048)  # Capture data
     sdrdata.close()  # Close SDR before the next iteration
     
-    filename = f"{save_path}lab_1_data_480khz_{int(rate / 1e3)}khz_offset.npz"
+    filename = f"{save_path}lab_1_data_480khz_{int(rate / 1e3)}khz.npz"
 
     np.savez(filename, time_data = time_data, sample_rate = rate, offset = offset)
 
