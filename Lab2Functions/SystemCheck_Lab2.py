222c409540c6a7fdcce7c4b01ce4c50a72528067
RicValle <157560430+RicValle@users.noreply.github.com>
2025-02-11 15:09:59 -0800

    Update and rename SignalSampler_Lab2.py to SystemCheck_Lab2.py

--- Lab2Functions/SystemCheck_Lab2.py
+++ Lab2Functions/SystemCheck_Lab2.py
@@ -2,14 +2,6 @@ import numpy as np
 import ugradio
 import matplotlib.pyplot as plt
 
-# 1. Time Conversions
-
-print("Local Time:", ugradio.timing.local_time())
-print("UTC Time:", ugradio.timing.utc())
-print("Unix Time:", ugradio.timing.unix_time())
-print("Julian Date:", ugradio.timing.julian_date())
-print("Local Sidereal Time (LST):", ugradio.timing.lst())
-
 # 2. System Checks
 
 def check_signal_levels():
